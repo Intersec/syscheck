1f1617d0d3a5e44cfcd5a8e98517955d81096673
Antoine LOHEAC <lewei.genell@common.aleeas.com>
2024-01-25 11:57:36 +0100

    Remove function is_key_set from generic_tools
    This function is now implemented in the db_tools.

--- generic_tools.py
+++ generic_tools.py
@@ -1,21 +1,3 @@
-def is_key_set(task, requirement, args):
-    arg_db = "env"
-    arg_key = requirement["id"]
-
-    if len(args) >= 2:
-        arg_key = args[1]
-
-    if len(args) >= 1:
-        arg_env = args[0]
-
-    # TODO add test to check that default values are used (they are not!)
-    if args[0] == "env":
-        db = task.get_env_key_value_db()
-    elif args[0] == "common":
-        db = task.get_workspace_key_value_db()
-
-    return db.is_key_set(arg_key)
-
 def each(task, requirement, args):
     for arg in args:
         if not arg:
