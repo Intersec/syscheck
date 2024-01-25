732e9e4f12ea700381a49324bb8aef45181c859a
Antoine LOHEAC <lewei.genell@common.aleeas.com>
2024-01-25 11:57:36 +0100

    Clean tests for class Task
    Add a private function in the test_task class to get a task using the
    default task collection.
    
    A part of the test_task tests is about creating a task and these tests
    were not modified.
    
    The rest of the tests, on another hand, is about what happens after the
    task is created so there is no need to itemize each steps of creating
    that task.

--- test_task.py
+++ test_task.py
@@ -22,6 +22,12 @@ class TestTask(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def _get_task(self, task_name_from_cfg):
+        task_path = self.task_cfg_path[task_name_from_cfg]
+        self.environment.key_value_db.set_value("task_conf_path", task_path)
+
+        return Task(self.workspace, self.environment)
+
     def test_error_if_configuration_path_not_set(self):
         expected_msg_re = f"^missing key 'task_conf_path' in kv database$"
         with self.assertRaisesRegex(AssertionError, expected_msg_re):
@@ -70,55 +76,37 @@ class TestTask(unittest.TestCase):
         self.assertTrue(True)
 
     def test_get_target_requirement_name(self):
-        self.environment.key_value_db.set_value("task_conf_path",
-                                                self.task_cfg_path['task-001'])
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         self.assertEqual(task.get_target_requirement_name(),
                          "TEST_TASK_001__REQ_TRUE")
 
     def test_get_requirement(self):
-        self.environment.key_value_db.set_value("task_conf_path",
-                                                self.task_cfg_path['task-001'])
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         req = task.get_requirement("TEST_TASK_001__REQ_TRUE")
         self.assertEqual(req["id"], "TEST_TASK_001__REQ_TRUE")
         self.assertEqual(req["label"], "Automatic check returns true")
         self.assertEqual(req["automatic_check"][0], "generic_tools.true")
 
     def test_is_ready_return_true(self):
-        self.environment.key_value_db.set_value("task_conf_path",
-                                                self.task_cfg_path['task-001'])
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         self.assertTrue(task.is_task_ready())
 
     def test_is_ready_return_false(self):
-        self.environment.key_value_db.set_value("task_conf_path",
-                                                self.task_cfg_path['task-002'])
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-002")
         self.assertFalse(task.is_task_ready())
 
     def test_is_ready_throws_not_boot(self):
-        self.environment.key_value_db.set_value("task_conf_path",
-                                                self.task_cfg_path['task-005'])
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-005")
         with self.assertRaises(NotBool):
             task.is_task_ready()
 
     def test_dependency_is_true_but_auto_check_is_false(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
-
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         self.assertFalse(
             task.check_requirement_status("TEST_TASK_001__REQ_DEP_TRUE"))
 
     def test_auto_check_is_not_a_function(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
-
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
 
         fun = "TEST_TASK_001__REQ_FALSE"
         expected_msg_re = f"^Cannot extract module and function from '{fun}'$"
@@ -127,61 +115,37 @@ class TestTask(unittest.TestCase):
                 "TEST_TASK_001__REQ_INVAL_AUTO_CHECK_001")
 
     def test_dependency_is_not_a_list(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
+        task = self._get_task("task-001")
         req_name = "TEST_TASK_001__REQ_INVAL_DEP_001"
-
-        task = Task(self.workspace, self.environment)
 
         expected_msg_re = f"^Dependencies for '{req_name}' is not an array$"
         with self.assertRaisesRegex(InvalidConfiguration, expected_msg_re):
             task.check_requirement_status(req_name)
 
     def test_dependency_do_not_exists(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
-
-        task = Task(self.workspace, self.environment)
-
+        task = self._get_task("task-001")
         expected_msg_re = f"^Requirement not found 'REQ_DO_NOT_EXISIS'$"
         with self.assertRaisesRegex(InvalidConfiguration, expected_msg_re):
             task.check_requirement_status("TEST_TASK_001__REQ_INVAL_DEP_002")
 
     def test_requirement_do_not_exists(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
-
-        task = Task(self.workspace, self.environment)
-
+        task = self._get_task("task-001")
         expected_msg_re = f"^Requirement not found 'REQ_DO_NOT_EXISIS'$"
         with self.assertRaisesRegex(InvalidConfiguration, expected_msg_re):
             task.check_requirement_status("REQ_DO_NOT_EXISIS")
 
     def test_auto_check_is_true_but_dependency_is_false(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
-
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         self.assertFalse(
             task.check_requirement_status("TEST_TASK_001__REQ_DEP_FALSE"))
 
     def test_both_dependencies_and_auto_check_are_true(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path",
-            self.task_cfg_path['task-001'])
-
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         self.assertTrue(
             task.check_requirement_status(
                 "TEST_TASK_001__REQ_DEP_AND_CHECK_TRUE"))
 
     def test_auto_check_returns_none(self):
-        self.environment.key_value_db.set_value(
-            "task_conf_path", self.task_cfg_path["task-001"])
-        task = Task(self.workspace, self.environment)
+        task = self._get_task("task-001")
         with self.assertRaises(NotBool):
             task.check_requirement_status("TEST_TASK_001__REQ_NO_RESULT")
