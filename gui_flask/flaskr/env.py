673b11b318b48323edd2348383eb7747c091b4ba
Antoine LOHEAC <lewei.genell@common.aleeas.com>
2024-01-25 11:57:36 +0100

    Protect access to environment page
    The environment's tree page can be accessed only if an environment was
    selected.

--- gui_flask/flaskr/env.py
+++ gui_flask/flaskr/env.py
@@ -1,9 +1,30 @@
 import functools
 
-from flask import Blueprint, render_template
+from flask import (Blueprint,
+                   render_template,
+                   session,
+                   flash,
+                   url_for,
+                   Markup,
+                   redirect)
 
 blueprint = Blueprint('env', __name__, url_prefix='/env')
 
+def env_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if not session["env_name"]:
+            select_env_url = url_for('select_env.page_select_env')
+            link = "<a href={select_env_url}>select one first</a>"
+            flash(Markup(f"No environment selected, {link}"), "warning")
+
+            return redirect(url_for('home.page_home'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 @blueprint.route('/tree', methods=["GET"])
+@env_required
 def page_env_tree():
     return render_template('env_tree.html')
